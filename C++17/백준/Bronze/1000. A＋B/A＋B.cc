3476b0f333c29d89080c7046c580fe51adf3a71a
Yong-Hun Suh <62800337+YONGHUNI@users.noreply.github.com>
2025-09-16 14:51:40 +0200

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int add(int x, int y){
+
+    return x+y;
+
+}
+
+int main(void){
+
+    int x,y,z;
+
+
+    cin>>x>>y;
+    
+    z=add(x,y);
+
+    cout<<z;
+
+}
+
+
